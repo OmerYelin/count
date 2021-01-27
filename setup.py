ed656a67777d2ac4c415ac566444f0d701e617ad
OmerYelin <76852454+OmerYelin@users.noreply.github.com>
2021-01-27 09:03:38 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'calculator-py',
+  packages = ['calculator-py'],
+  version = '1.0.0',
+  license='MIT',
+  description = 'Very easy to use, you can just calculate.',
+  author = 'Omer Yelin',
+  author_email = 'iggy030706@gmail.com',
+  url = 'https://github.com/OmerYelin/calculator-py',
+  download_url = 'https://github.com/OmerYelin/calculator-py/archive/1.0.0.tar.gz',
+  keywords = ['calculator', 'numbers', 'math'],
+  install_requires=[
+          'sys',
+          
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',  
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
